3fb80cc60e72a2fed82a5f4141d2b6a2491e6bf1
Dnstarr-ctrl <divi2k21@gmail.com>
2026-06-05 21:13:00 +0530

    calc

--- calc.py
+++ calc.py
@@ -2,10 +2,10 @@ def add(n1,n2):
     return n1+n2
 def subtract(n3,n4):
     return n3-n4
-def multiply(n5,n6):
-    return n5*n6
-def divide(n7,n8):
-    return n7/n8
+def multiply(n5,b5):
+    return n5*b5
+def divide(n7,a3):
+    return n7/a3
 useroperatorchoose=input("Please enter the operation to perform: A) Add B) Subtract C) Multiply D) Divide: ").upper()
 numchoosing=int(input("Enter the 1st number:"))
 numchoosing2=int(input("Enter the 2nd number:"))
