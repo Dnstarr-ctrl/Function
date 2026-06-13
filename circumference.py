d5eab21916f175cbd6e3c775d5a638a72ab92652
Dnstarr-ctrl <divi2k21@gmail.com>
2026-06-13 17:31:20 +0530

    circumference calculator

--- circumference.py
+++ circumference.py
@@ -1,5 +1,9 @@
 def circumferencecalc(radius):
     if radius < 0:
-        print("circumference is ", circumference )
+        print()
+r=int(input("Enter the radius:- "))
+circumference=2*3.14*r
+print("the circumference is: ", circumference)
+
 
 
